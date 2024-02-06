ce4d32bf1970accb82ff012ce57f76546bc85dd5
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-06 23:38:43 +0600

    layout content changing

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,26 +1,34 @@
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Layout, Menu } from "antd";
-import { createElement } from "react";
+import { Layout, Menu, MenuProps } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
 
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  UserOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+const items: MenuProps["items"] = [
+  {
+    key: "dashboard",
+    label: "Dashboard",
+  },
+  {
+    key: "profile",
+    label: "Profile",
+  },
+  {
+    key: "rest",
+    label: "Rest",
+    children: [
+      {
+        key: "item1",
+        label: "Item One",
+      },
+      {
+        key: "item2",
+        label: "Item Two",
+      },
+    ],
+  },
+];
 
 const MainLayout = () => {
   return (
-    <Layout>
+    <Layout style={{ height: "100vh" }}>
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
@@ -31,7 +39,20 @@ const MainLayout = () => {
           console.log(collapsed, type);
         }}
       >
-        <div className="demo-logo-vertical" />
+        <div
+          style={{
+            color: "white",
+            textAlign: "center",
+            height: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontWeight: "bold",
+            fontSize: "24px",
+          }}
+        >
+          <h2>PH Unique</h2>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
