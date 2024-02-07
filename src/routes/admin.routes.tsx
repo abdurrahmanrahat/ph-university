4cffa1d5b378b34500d3880731b3ca4887eb252e
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-07 19:31:26 +0600

    programmatically sidebar items handle

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { NavLink } from "react-router-dom";
 import AdminDashboard from "../pages/admin/AdminDashboard/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty/CreateFaculty";
@@ -7,6 +8,12 @@ import CreateStudent from "../pages/admin/CreateStudent/CreateStudent";
 type TRoute = {
   path: string;
   element: ReactNode;
+};
+
+type TSidebarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TSidebarItem[];
 };
 
 const adminPaths = [
@@ -58,6 +65,33 @@ export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
 
   return acc;
 }, []);
+
+// programmatically sidebar items handle
+
+export const adminSidebarItems = adminPaths.reduce(
+  (acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+
+    return acc;
+  },
+  []
+);
 
 // hard way
 
