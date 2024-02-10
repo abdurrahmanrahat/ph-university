0800da731eb70d99bfe7e07657a0fd20bb7fd753
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-10 21:42:23 +0600

    get all academic semeters

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,7 +1,7 @@
-import AdminDashboard from "../pages/admin/AdminDashboard/AdminDashboard";
-import CreateAdmin from "../pages/admin/CreateAdmin/CreateAdmin";
-import CreateFaculty from "../pages/admin/CreateFaculty/CreateFaculty";
-import CreateStudent from "../pages/admin/CreateStudent/CreateStudent";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import CreateAdmin from "../pages/admin/CreateAdmin";
+import CreateFaculty from "../pages/admin/CreateFaculty";
+import CreateStudent from "../pages/admin/CreateStudent";
 
 export const adminPaths = [
   {
