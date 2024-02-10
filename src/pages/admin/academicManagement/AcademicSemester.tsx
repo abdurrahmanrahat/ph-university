d8af840b6f646df46b965a2bf8ca5eaa67c1dd57
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-10 22:19:14 +0600

    send token in backed

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,4 +1,10 @@
+import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
+
 const AcademicSemester = () => {
+  const { data } = useGetAllSemestersQuery(undefined);
+
+  console.log(data);
+
   return <div>AcademicSemester</div>;
 };
 
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,24 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include", // include sessionToken in cookie
+  // send token in every query / mutation.
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+
+    return headers;
+  },
+});
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
-    credentials: "include", // include sessionToken in cookie
-  }),
+  baseQuery: baseQuery,
+
   endpoints: () => ({}),
 });
