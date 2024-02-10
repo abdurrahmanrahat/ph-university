4f6f8b2da7637ea7fe11a8c84b56e64143543923
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-10 15:10:28 +0600

    login form added

---
+++ src/pages/Login.tsx
@@ -0,0 +1,27 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+
+const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">Id:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
+};
+
+export default Login;
