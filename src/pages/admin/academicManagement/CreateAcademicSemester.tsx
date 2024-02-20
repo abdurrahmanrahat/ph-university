16f7263f33f070a6038a9102e6b0ef516d4715a4
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-20 20:46:26 +0600

    form handle from antd

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
@@ -9,10 +9,15 @@ const CreateAcademicSemester = () => {
   };
 
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" />
-      <Button htmlType="submit"></Button>
-    </PHForm>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="name" label="Name:" />
+          <PHInput type="text" name="year" label="Year:" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
