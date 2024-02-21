061e5057589859c4b7e617f852d034fe79447b52
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-21 15:21:18 +0600

    Show Semester Data Using Ant Design Table

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,11 +1,77 @@
+import { Table, TableColumnsType, TableProps } from "antd";
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
 
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
+
 const AcademicSemester = () => {
-  const { data } = useGetAllSemestersQuery(undefined);
+  const { data: semesters } = useGetAllSemestersQuery(undefined);
 
-  console.log(data);
+  const tableData = semesters?.data.map((item) => ({
+    _id: item._id,
+    name: item.name,
+    startMonth: item.startMonth,
+    endMonth: item.endMonth,
+    year: item.year,
+  }));
 
-  return <div>AcademicSemester</div>;
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      filters: [
+        {
+          text: "Joe",
+          value: "Joe",
+        },
+        {
+          text: "Jim",
+          value: "Jim",
+        },
+        {
+          text: "Submenu",
+          value: "Submenu",
+          children: [
+            {
+              text: "Green",
+              value: "Green",
+            },
+            {
+              text: "Black",
+              value: "Black",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Year",
+      dataIndex: "year",
+    },
+    {
+      title: "Start Month",
+      dataIndex: "startMonth",
+    },
+    {
+      title: "End Month",
+      dataIndex: "endMonth",
+    },
+  ];
+
+  const onChange: TableProps<DataType>["onChange"] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
+
+  return <Table columns={columns} dataSource={tableData} onChange={onChange} />;
 };
 
 export default AcademicSemester;
